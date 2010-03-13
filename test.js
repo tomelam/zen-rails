18e65423b837f8360c66c086945b092701cb6c5c
Tom Elam <tomelam@gmail.com>
2010-03-14 02:01:31 +0530

    Added startup function to start up all widgets.

--- test.js
+++ test.js
@@ -28,43 +28,108 @@
     c = createNew(Baz, 3, 4);
     console.debug('c.a => ' + c.a);
 
-    var testArray = [["div", []]];
-    var testArray2 =
-    [["div", [["table", [["tr", [["td", []], ["td", []]]],
-			 ["tr", [["td", []], ["td", []]]],
-			 ["tr", [["td", []], ["td", []]]]]],
-	      ["p", [["span", [["div", []], ["div", []]]],
-		     ["div", []]]]]],
-     ["table", [["tr", [["td", []], ["td", []]]],
-		["tr", [["td", []], ["td", []]]],
-		["tr", [["td", []], ["td", []]]]]]];
-    var testSubtree = ["div", []];
-    var testSubtree2 = ["div", testArray];
-    var testSubtree3 = ["div", testArray2];
-    var testSubtree4 = ["div", [["p", []], ["dijit.layout.ContentPane", []]]];
-    var testSubtree5 =
-    ["div", [["p", []],
-	     ["dijit.layout.ContentPane",
-	      [["dijit.layout.ContentPane", [["div", []]]]]]]];
-    var testSubtree6 =
-    ["div", [["dijit.layout.AccordionContainer", [["dijit.layout.AccordionPane",
-						   ["div", []]],
-						  ["dijit.layout.AccordionPane", []]]]]];
+    var tree1 =
+    ["div", {style:{width:'180px',height:'180px',backgroundColor:'red'},
+	     title:'Title'}, []];
+    var tree2 =
+    ["div", {style:{width:'200px',height:'200px',backgroundColor:'red'}},
+     [["div",
+       {style:{width:'180px',height:'180px',backgroundColor:'orange'},
+	title:'Title 1'}, []]]];
+    var tree3 =
+    ["div", {style:{width:'200px',height:'250px',backgroundColor:'red'}},
+     [["div",
+       {}, []],
+      ["table",
+       {style:{backgroundColor:'yellow'}, title:'Title 3'},
+       [["tr",
+	 {style:{height:'50px'}},
+	 [["td", {style:{width:'50px'}}, []],
+	  ["td", {style:{width:'50px'}}, []]]],
+	["tr",
+	 {style:{height:'60px'}},
+	 [["td", {style:{width:'50px'}}, []],
+	  ["td", {style:{width:'50px'}}, []]]],
+	["tr",
+	 {style:{height:'50px'}},
+	 [["td", {style:{width:'50px'}}, []],
+	  ["td", {style:{width:'50px'}}, []]]]]],
+      ["p", {}, [["span", {}, [["div", {}, []], ["div", {}, []]]],
+		  ["div", {}, []]]]]];
+    var tree4 =
+    ["div", {},
+     [["p", {}, []],
+      ["dijit.layout.ContentPane",
+       {style:{width:"300px",height:"300px",backgroundColor:"red"}},
+       []]]];
+    var tree5 =
+    ["div", {},
+     [["p", {}, []],
+      ["dijit.layout.ContentPane",
+       {"class":"box"},
+       []]]];
+    var tree6 =
+    ["div", {},
+     [["p", {}, []],
+      ["dijit.layout.ContentPane",
+       {style:{width:"300px",height:"140px",backgroundColor:"red"}},
+       [["dijit.layout.ContentPane",
+	 {"class":"box"},
+	 [["div", {}, []]]]]]]];
+    var tree7 =
+    ["div", {},
+     [["dijit.layout.AccordionContainer",
+       {style:{width:"100%",height:"160px",backgroundColor:"yellow"}},
+       [["dijit.layout.AccordionPane",
+	 {title:"pane 1"},
+	 [["div", {style:{width:"100%",height:"140px",
+			  backgroundColor:"orange"}}, []]]],
+	["dijit.layout.AccordionPane",
+	 {title:"pane 2"},
+	 [["div", {style:{width:"100%",height:"140px",backgroundColor:"red"}},
+	   []]]]]]]];
+    var tree8 =
+    ["div", {},
+     [["dijit.layout.AccordionContainer",
+       {style:{width:"100%",height:"160px",backgroundColor:"yellow"}},
+       [["dijit.layout.ContentPane",
+	 {title:"pane 1"},
+	 [["div", {style:{width:"100%",height:"140px",
+			  backgroundColor:"orange"}}, []]]],
+	["dijit.layout.ContentPane",
+	 {title:"pane 2"},
+	 [["div", {style:{width:"100%",height:"140px",backgroundColor:"red"}},
+	   []]]]]]]];
+    var tree9 =
+    ["div", {},
+     [["dijit.layout.AccordionContainer",
+       {style:{width:"100%",height:"400px",backgroundColor:"yellow"}},
+       [["dijit.layout.ContentPane",
+	 {title:"pane 1"},
+	 [["div", {style:{width:"100%",height:"140px",
+			  backgroundColor:"orange"}},
+	   [["dijit.layout.AccordionContainer",
+	     {style:{width:"100%",height:"160px",backgroundColor:"yellow"}},
+	     [["dijit.layout.ContentPane",
+	       {title:"pane 1"},
+	       [["div", {style:{width:"100%",height:"140px",
+				backgroundColor:"orange"}}, []]]],
+	      ["dijit.layout.ContentPane",
+	       {title:"pane 2"},
+	       [["div", {style:{width:"100%",height:"140px",
+				backgroundColor:"red"}},
+		 []]]]]]]]]],
+	["dijit.layout.ContentPane",
+	 {title:"pane 2"},
+	 [["div", {style:{width:"100%",height:"140px",backgroundColor:"red"}},
+	   []]]]]]]];
 		
-    var newComponent;
-    test = function() {
-	newComponent = zen.createSubtree(testSubtree6);
-	newComponent.appendMyselfToParent(document.body);
+    test = function(tree) {
+	console.debug("Testing creation of a tree");
+	var newComponent;
+	newComponent = zen.createSubtree(tree);
+	console.debug('newComponent => ' + newComponent);
+	newComponent.appendMyselfToParent(dojo.body());
+	zen.startup();
+	console.debug("Done");
     };
-
-    var b, cp, tp;
-    testDijits = function() {
-	b = new dijit.form.Button({ "class": "large" }, dojo.byId("id0"));
-	cp = new dijit.layout.ContentPane({ "class": "box" },
-					  dojo.byId("id1"));
-	tp = new dijit.TitlePane( {title:"Inner Pane"}, dojo.byId("id2"));
-    }
-
-    testMyDijit = function() {
-	return zen.dojo.createDijit(dijit.layout.ContentPane);
-    }
--- zen.js
+++ zen.js
@@ -1,18 +1,18 @@
     zen = {
 	createSubtree : function(treeSpec) {
 	    var i, rule, parentCompon, compon, len, constructor, parentDomNode,
-		componKind = treeSpec[0], initParms, subtree = treeSpec[1];
+		componKind = treeSpec[0],
+		initParms = treeSpec[1],
+		subtree = treeSpec[2];
 	    rule = zen.invertedRulesTable[componKind];
 	    console.debug('rule = > ' + rule + ', componKind => ' +
 			  componKind);
 	    constructor = zen.rule2ref(rule);
-	    console.debug('constructor => ' + constructor);
-	    parentCompon = constructor.call(document, componKind);
+	    parentCompon = constructor.call(document, componKind, initParms);
 	    console.debug('parentCompon => ' + parentCompon);
 	    len = subtree.length;
 	    for (i=0; i<len; i++) {
 		compon = zen.createSubtree(subtree[i]);
-		console.dir('compon => ' + compon);
 		compon.appendMyselfToParent(parentCompon.getDomNode());
 	    };
 	    return parentCompon;
@@ -51,19 +51,22 @@
 		}
 	    }
 	    return eval(rule);
+	},
+	widgets : [],
+	startup : function() {
+	    // Start up all the Dojo widgets. The order is important.
+	    dojo.forEach(this.widgets.reverse(), function(w) { w.startup(); });
 	}
     };
-    // zen.createDijit does not allow a dijit to be built on a
+    // Zen.createDijit does not allow a dijit to be built on a
     // passed-in HTML element node. Instead, the dijit constructor is
     // called without reference to a node, thus causing it to create a
-    // top node on the fly. dijit can be added to a parent component
-    // afterwards.
+    // top node on the fly. The dijit can be added to a parent
+    // component afterwards.
     zen.createDijit = function(klass, initParms) {
 	console.debug('zen.dojo.createDijit, klass => ' + klass);
 	dojo.require(klass);
-	//widget = createNew(zen.rule2ref(klass), {'class':'box'},
-	//	                 dojo.byId('id2'));
-	widget = createNew(zen.rule2ref(klass), {'class':'ac'});
+	widget = createNew(zen.rule2ref(klass), initParms);
 	console.debug('widget => ' + widget);
 	widget.children = [];
 	// FIXME: Check whether this works when the parent is a dijit.
@@ -71,16 +74,24 @@
 	widget.appendMyselfToParent = function(parent) {
 	    console.debug('appendMyselfToParent: this => ' + this);
 	    parent.getDomNode().appendChild(this.domNode);
-	    // parent.children.push(widget);
 	};
 	widget.getDomNode = function() {
 	    return this.domNode;
 	};
 	widget.kind = klass;
+	zen.widgets.push(widget);
 	return widget;
     };
+    zen.createElement = function(kind, attributes) {
+	console.debug('zen.createElement: kind => ' + kind +
+		      ', attributes => ' + attributes);
+	var element = document.createElement(kind);
+	console.debug('element => ' + element);
+	dojo.attr(element, attributes);
+	return element;
+    };
     zen.shortCutsTable = {
-	createElement : document.createElement,
+	createElement : zen.createElement,
 	createTextNode : document.createTextNode,
 	createDijit : zen.createDijit
     };
@@ -88,7 +99,6 @@
     Element.addMethods( // Extend all HTML elements with these methods.
 	{
 	    appendMyselfToParent : function (element, parent) {
-		dojo.addClass(element, 'box');
 		console.debug('appendMyselfToParent: element => ' +
 			      element + ', parent => ' + parent);
 		parent.appendChild(element);

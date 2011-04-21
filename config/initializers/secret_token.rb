94af95d377bf6b1aa9ec270f32e82050cb63dc45
Tom Elam <tomelam@gmail.com>
2011-04-21 15:11:17 +0530

    Add a Rails backend to Zen.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZenRails::Application.config.secret_token = '7af38f32e75f84799c7b1ddc4c5d9298974e645379fb4776d0e1da01be9ba2b07b481192057f0cb44273624134dfcb2580325c011fe7f6a948033ce95127f558'

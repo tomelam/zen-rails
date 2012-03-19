99350c054acc01afb0f2bd1832208c7e54c93b7e
Tom Elam <tomelam@gmail.com>
2012-03-19 20:35:13 +0530

    Passes 5 of 6 CSS URL fixing tests using a much simpler regex.

--- public/javascripts/zen/test_cssfixer.js
+++ public/javascripts/zen/test_cssfixer.js
@@ -9,43 +9,43 @@ tests.register(
 	{
 	    name: "1. Do not convert a one-property CSS declaration without a URL",
 	    runTest: function() {
-		var fixedDecl = zen.fixCssDeclUrl('margin:0;', 'example.com');
-		doh.assertEqual('margin:0;', fixedDecl);
+		var fixedDecl = zen.fixCssDeclUrl('margin:0', 'example.com');
+		doh.assertEqual('margin:0', fixedDecl);
 	    }
 	},
 	{
 	    name: "2. Do not convert a 3-property CSS declaration without a URL",
 	    runTest: function() {
 		var fixedDecl = zen.fixCssDeclUrl('margin:0;overflow-y:scroll;padding:3px 8px 0', 'example.com');
-		doh.assertEqual('margin:0;overflow-y:scroll;padding:3px 8px 0;', fixedDecl);
+		doh.assertEqual('margin:0;overflow-y:scroll;padding:3px 8px 0', fixedDecl);
 	    }
 	},
 	{
 	    name: "3. Do not convert an absolute-path URL",
 	    runTest: function() {
-		var fixedDecl = zen.fixCssDeclUrl('background:url("http://example.com/foo.png") repeat;', 'example.com');
-		doh.assertEqual('background:url("http://example.com/foo.png") repeat;', fixedDecl);
+		var fixedDecl = zen.fixCssDeclUrl('background:url("http://example.com/foo.png") repeat', 'example.com');
+		doh.assertEqual('background:url("http://example.com/foo.png") repeat', fixedDecl);
 	    }
 	},
 	{
 	    name: "4. Convert a protocol relative URL to an absolute-path URL",
 	    runTest: function() {
 		var fixedDecl = zen.fixCssDeclUrl('background:url("//example.com/foo.png") repeat', 'example.com');
-		doh.assertEqual('background:url("http://example.com/foo.png") repeat;', fixedDecl);
+		doh.assertEqual('background:url("http://example.com/foo.png") repeat', fixedDecl);
 	    }
 	},
 	{
 	    name: "5. Convert relative-path URL to absolute-path URL",
 	    runTest: function() {
-		var fixedDecl = zen.fixCssDeclUrl('background:url("/foo.png");', 'example.com');
-		doh.assertEqual('background:url("http://example.com/foo.png");', fixedDecl);
+		var fixedDecl = zen.fixCssDeclUrl('background:url("/foo.png")', 'example.com');
+		doh.assertEqual('background:url("http://example.com/foo.png")', fixedDecl);
 	    }
 	},
 	{
 	    name: "6. Do not convert a 'data' URL (IETF RFC 2397)",
 	    runTest: function() {
 		var fixedDecl = zen.fixCssDeclUrl('background: url("data:image/gif;base64,R0lGODlhCgAEAMIEAP9BGP6pl") transparent; margin:0', 'example.com');
-		doh.assertEqual('background: url("data:image/gif;base64,R0lGODlhCgAEAMIEAP9BGP6pl") transparent;margin:0;', fixedDecl);
+		doh.assertEqual('background: url("data:image/gif;base64,R0lGODlhCgAEAMIEAP9BGP6pl") transparent;margin:0', fixedDecl);
 	    }
 	}
     ]);
